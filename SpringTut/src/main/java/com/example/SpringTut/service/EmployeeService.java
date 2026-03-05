51f5e5ba6027ec2c6a12c51e0fc210494c12a21c
kyanh180320 <54309137+kyanh180320@users.noreply.github.com>
2026-03-06 00:09:19 +0700

    Update

--- SpringTut/src/main/java/com/example/SpringTut/service/EmployeeService.java
+++ SpringTut/src/main/java/com/example/SpringTut/service/EmployeeService.java
@@ -2,7 +2,6 @@ package com.example.SpringTut.service;
 
 import com.example.SpringTut.model.Employee;
 import com.example.SpringTut.repository.EmployeeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
